4c494eb32e2ad2e40f57a385aeeac18eca2b7677
Vinicius Dornelas <vinicius.s.dornelas@gmail.com>
2026-02-07 14:59:00 -0300

    fix: return correct user type

--- Modules/User/Http/Resources/UserResource.php
+++ Modules/User/Http/Resources/UserResource.php
@@ -15,7 +15,7 @@ class UserResource extends JsonResource
             'name'   => $this->name,
             'email'  => $this->email,
             'status' => $this->status,
-            'type'   => $this->type === 'admin' ? 'Administrador' : 'Vendedor',
+            'type'   => $this->type,
             'created_at' => $this->created_at,
             'updated_at' => $this->updated_at,
         ];
